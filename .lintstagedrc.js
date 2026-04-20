9e30cd951e36c8da9645e782e27dc1ea564c2210
baditu <baditaandrei20@gmail.com>
2026-04-20 11:32:26 +0300

    solve the PR

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -6,11 +6,22 @@ const buildNextEslintCommand = (filenames) =>
     .join(" --file ")}`;
 
 const checkTypesNextCommand = () => "yarn next:check-types";
+const buildFoundryFmtCommand = (filenames) =>
+  `yarn workspace @sh/foundry exec forge fmt ${filenames
+    .map((f) => path.relative(path.join("packages", "foundry"), f))
+    .join(" ")}`;
+const buildFoundryLintCommand = (filenames) =>
+  `yarn workspace @sh/foundry exec forge fmt --check ${filenames
+    .map((f) => path.relative(path.join("packages", "foundry"), f))
+    .join(" ")}`;
 
 module.exports = {
   "packages/nextjs/**/*.{ts,tsx}": [
     buildNextEslintCommand,
     checkTypesNextCommand,
   ],
-  "packages/foundry/**/*.sol": ["yarn foundry:format", "yarn foundry:lint"],
+  "packages/foundry/**/*.sol": [
+    buildFoundryFmtCommand,
+    buildFoundryLintCommand,
+  ],
 };
